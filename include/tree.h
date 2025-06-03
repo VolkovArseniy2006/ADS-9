f045d065d4385b10fcf3109b870e95dc1ee4abef
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-06-03 16:31:00 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -10,21 +10,16 @@ class PMTree {
   struct Node {
     char value;
     std::vector<std::unique_ptr<Node>> children;
-    
     explicit Node(char val) : value(val) {}
   };
-
   explicit PMTree(const std::vector<char>& elements);
   ~PMTree() = default;
-
   std::vector<std::vector<char>> getAllPerms() const;
   std::vector<char> getPerm1(int num) const;
   std::vector<char> getPerm2(int num) const;
-
  private:
   std::unique_ptr<Node> root;
   int totalPermutations;
-  
   void buildTree(Node* parent, const std::vector<char>& remaining);
   void collectPerms(const Node* node, std::vector<char>& current,
                    std::vector<std::vector<char>>& result) const;
@@ -34,9 +29,7 @@ class PMTree {
                           std::vector<char>& result) const;
   int factorial(int n) const;
 };
-
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
 std::vector<char> getPerm1(const PMTree& tree, int num);
 std::vector<char> getPerm2(const PMTree& tree, int num);
-
 #endif  // INCLUDE_TREE_H_
